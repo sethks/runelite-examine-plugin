455057f28972718793e2290713902150e315f9b0
odezro21@gmail.com <odezro21@gmail.com>
2024-05-03 16:11:58 -0400

    Readme change, stat function fix

--- src/main/java/com/monsterexamine/MonsterExamineConfig.java
+++ src/main/java/com/monsterexamine/MonsterExamineConfig.java
@@ -7,13 +7,5 @@ import net.runelite.client.config.ConfigItem;
 @ConfigGroup("Monster Examine")
 public interface MonsterExamineConfig extends Config
 {
-	@ConfigItem(
-		keyName = "Monster Examine",
-		name = "Monster Examine",
-		description = "Show Monster's Weaknesses"
-	)
-	default boolean monsterExamine()
-	{
-		return false;
-	}
+
 }
